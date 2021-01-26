250b9f95584db8e5bebb9b304860b6fec581039b
AJ <amosjunior_jr@hotmail.com>
2021-01-26 11:21:19 -0300

    Inital commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { json } from 'express';
+
+const app = express();
+
+app.use(json());
+
+app.get('/', (req, res) => {
+    return res.json({ message: "Hello World!" });
+})
+
+app.listen(8080, () => {
+    console.log('✔ Server running on port 8080!');
+});
